8c016ca4ed5babd0c67c22cf419e50c36e405a08
Aarni Koskela <akx@iki.fi>
2024-10-15 14:30:36 +0300

    Soft-import trainer

--- model/__init__.py
+++ model/__init__.py
@@ -1,7 +1,12 @@
+import warnings
+
 from model.cfm import CFM
 
 from model.backbones.unett import UNetT
 from model.backbones.dit import DiT
 from model.backbones.mmdit import MMDiT
 
-from model.trainer import Trainer
+try:
+    from model.trainer import Trainer
+except Exception as e:
+    warnings.warn(f"Unable to import trainer module: {e}")
