943337c577a8dbf7718869545afc1510315fde0b
Roberts Slisans <rsxdalv@gmail.com>
2024-10-17 18:02:20 +0300

    simplify demo

--- package_demo.py
+++ package_demo.py
@@ -1,26 +1,7 @@
 import gradio as gr
-from f5_tts.gradio_app import app_tts, app_podcast, app_emotional, app_credits
+from f5_tts.gradio_app import app
 
-with gr.Blocks() as app:
-    gr.Markdown(
-        """
-# E2/F5 TTS
-
-This is a local web UI for F5 TTS with advanced batch processing support. This app supports the following TTS models:
-
-* [F5-TTS](https://arxiv.org/abs/2410.06885) (A Fairytaler that Fakes Fluent and Faithful Speech with Flow Matching)
-* [E2 TTS](https://arxiv.org/abs/2406.18009) (Embarrassingly Easy Fully Non-Autoregressive Zero-Shot TTS)
-
-The checkpoints support English and Chinese.
-
-If you're having issues, try converting your reference audio to WAV or MP3, clipping it to 15s, and shortening your prompt.
-
-**NOTE: Reference text will be automatically transcribed with Whisper if not provided. For best results, keep your reference clips short (<15s). Ensure the audio is fully uploaded before generating.**
-"""
-    )
-    gr.TabbedInterface(
-        [app_tts, app_podcast, app_emotional, app_credits],
-        ["TTS", "Podcast", "Multi-Style", "Credits"],
-    )
-
-    app.launch()
+with gr.Blocks() as other_app:
+    app.render()
+    
+other_app.launch()
