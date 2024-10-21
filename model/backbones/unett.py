d15ef3679aa572453fc1d44f66947de71fa85e72
SWivid <swivid@qq.com>
2024-10-21 17:55:58 +0800

    fix address #191

--- model/backbones/unett.py
+++ model/backbones/unett.py
@@ -48,9 +48,9 @@ class TextEmbedding(nn.Module):
             self.extra_modeling = False
 
     def forward(self, text: int["b nt"], seq_len, drop_text=False):  # noqa: F722
-        batch, text_len = text.shape[0], text.shape[1]
         text = text + 1  # use 0 as filler token. preprocess of batch pad -1, see list_str_to_idx()
         text = text[:, :seq_len]  # curtail if character tokens are more than the mel spec tokens
+        batch, text_len = text.shape[0], text.shape[1]
         text = F.pad(text, (0, seq_len - text_len), value=0)
 
         if drop_text:  # cfg for text
