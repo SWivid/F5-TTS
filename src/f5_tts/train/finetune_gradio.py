f7aafc84138a7112a9c4c873ec363ced20ca9d46
Max <max@giller.dev>
2025-03-06 17:29:37 +0100

    Escaping spaces in finetune_gradio launch path

--- src/f5_tts/train/finetune_gradio.py
+++ src/f5_tts/train/finetune_gradio.py
@@ -432,7 +432,7 @@ def start_training(
         fp16 = ""
 
     cmd = (
-        f"accelerate launch {fp16} {file_train} --exp_name {exp_name}"
+        f"accelerate launch {fp16} \"{file_train}\" --exp_name {exp_name}"
         f" --learning_rate {learning_rate}"
         f" --batch_size_per_gpu {batch_size_per_gpu}"
         f" --batch_size_type {batch_size_type}"
